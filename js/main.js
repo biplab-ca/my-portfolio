01c6db51955c21b0afc625928c1b0474ac2ba2da
biplab-ca <biplabmazumder.ca@gmail.com>
2019-02-19 01:57:36 +0530

    Move static markup for work examples into React component

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
-console.log("Webpack works");
+const myWork = [
+  {
+    'title': "Cloud Portfolio",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "DevSecOps Portfolio",
+    'image': {
+      'desc': "example screenshot of a project involving devops",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "A Serverless Portfolio",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example3.png",
+      'comment': ""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
